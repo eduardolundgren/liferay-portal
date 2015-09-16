7673948b5d81ca1c1a68a483dc821c168078e83e
Miroslav Ligas <miroslav.ligas@liferay.com>
2015-09-16 11:46:48 +0100

    LPS-58633 SoyManager changed to support multi templates

--- modules/portal/portal-template-soy/src/com/liferay/portal/template/soy/SoyManager.java
+++ modules/portal/portal-template-soy/src/com/liferay/portal/template/soy/SoyManager.java
@@ -20,10 +20,12 @@ import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateManager;
 import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.template.BaseMultiTemplateManager;
 import com.liferay.portal.template.BaseTemplateManager;
 import com.liferay.portal.template.RestrictedTemplate;
 import com.liferay.portal.template.TemplateContextHelper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -37,7 +39,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {"language.type=" + TemplateConstants.LANG_TYPE_SOY},
 	service = TemplateManager.class
 )
-public class SoyManager extends BaseTemplateManager {
+public class SoyManager extends BaseMultiTemplateManager {
 
 	@Override
 	public void destroy() {
@@ -70,12 +72,12 @@ public class SoyManager extends BaseTemplateManager {
 
 	@Override
 	protected Template doGetTemplate(
-		TemplateResource templateResource,
+		List<TemplateResource> templateResources,
 		TemplateResource errorTemplateResource, boolean restricted,
 		Map<String, Object> helperUtilities, boolean privileged) {
 
 		Template template = new SoyTemplate(
-			templateResource, errorTemplateResource, helperUtilities,
+			templateResources, errorTemplateResource, helperUtilities,
 			new SoyFileSet.Builder(), templateContextHelper, privileged);
 
 		if (restricted) {
